22b4323eb80bd798662d1326a806500755bdf712
Connor James <connor20082003@gmail.com>
2025-03-12 21:10:11 -0300

    Continuação da página Dashboard

--- App.tsx
+++ App.tsx
@@ -1,8 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Routes } from './src/routes';
+import { styles } from './src/pages/Dashboard/styles';
+import { useFonts, Rubik_600SemiBold, Rubik_400Regular, Rubik_500Medium, Rubik_700Bold} from '@expo-google-fonts/rubik';
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts ({
+    Rubik_600SemiBold, 
+    Rubik_400Regular, 
+    Rubik_500Medium, 
+    Rubik_700Bold,
+  })
+
+  if (!fontsLoaded) {
+    return <Loading/>
+  }
+
   return (
     <View style={styles.container}>
       <Routes/>
@@ -11,9 +25,4 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'blue',
-  },
-});
+
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,20 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { styles } from './styles';
 
 export function Dashboard() {
   return (
     <View style={styles.container}>
-      <Text>Dashboard</Text>
+      <View>
+        <Text>Controle de Gastos</Text>
+      </View>
+      <TextInput placeholder='Descrição'/>
+      <TextInput placeholder='Data'/>
+      <TextInput placeholder='Valor'/>
+      <TextInput placeholder='Categoria'/>
+      <TextInput placeholder='Local'/>
+
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    // backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
